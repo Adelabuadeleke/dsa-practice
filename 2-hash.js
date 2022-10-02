2a4b5a9560d8b9629e4ed41ede5f7cc0a9e1e258
kamal <kamade123@gmail.com>
2022-10-02 19:47:01 +0100

    hash table intro

---
+++ 2-hash.js
@@ -0,0 +1,14 @@
+let user = {
+ age: 54,
+ name: 'Kyle',
+ magic:true,
+ scream: function(){
+  console.log('aaaaah!')
+ }
+}
+
+user.age
+user.spel = 'abra kadabra'; // 0(1)
+user.scream
+
+// O(n/k) --> collisions
