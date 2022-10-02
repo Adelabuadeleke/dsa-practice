d9faa948bc3e95c868ce2145af4727160ed1bdf0
kamal <kamade123@gmail.com>
2022-10-02 19:54:19 +0100

    hash table methods/options

--- 2-hash.js
+++ 2-hash.js
@@ -7,8 +7,11 @@ let user = {
  }
 }
 
-user.age
+user.age //O(1)
 user.spel = 'abra kadabra'; // 0(1)
-user.scream
+user.scream //O(1)
 
 // O(n/k) --> collisions
+
+const a = new Map()
+const b = Set()
