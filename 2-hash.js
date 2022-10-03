8d7d2d226c0d325b8db24cd4780063174a912adb
kamal <kamade123@gmail.com>
2022-10-03 01:22:03 +0100

    creating hash table

--- 2-hash.js
+++ 2-hash.js
@@ -15,3 +15,32 @@ user.scream //O(1)
 
 const a = new Map()
 const b = Set()
+
+// hash table
+class HashTable {
+ constructor(size) {
+  this.data = new Array(size);
+  [['grape', 100000]]
+ }
+ 
+  _hash(key) {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++){
+   hash = (hash + key.charCodeAt(i) * i) %
+   this.data.length;
+  }
+  return hash;
+ }
+
+ set() {
+
+ }
+
+ get() {
+  
+ }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.get('grape');
