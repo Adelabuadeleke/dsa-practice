7b571e7f4615f1d98327746c370f30088dcabba3
kamal <kamade123@gmail.com>
2022-10-04 20:14:22 +0100

    added append method to linkedlist

--- 3-linked-list.js
+++ 3-linked-list.js
@@ -22,8 +22,23 @@ class LinkedList {
   this.tail = this.head;
   this.length = 1;
  }
+
+ append(value) {
+
+
+  const newNode = {
+   value: value,
+   next: null
+  }
+
+  this.tail.next = newNode;
+  this.tail = newNode;
+  this.length++;
+  return console.log(this)
+ }
 }
 
 const myLinkedList = new LinkedList(10);
-
-console.log(myLinkedList);
+myLinkedList.append(5);
+myLinkedList.append(16);
+// console.log(myLinkedList);
