1767825a8c3319ebfc07a57ec21d277d95713ec0
kamal <kamade123@gmail.com>
2022-10-04 20:30:00 +0100

    added a Node class to use in  linkedlist

--- 3-linked-list.js
+++ 3-linked-list.js
@@ -12,22 +12,23 @@
 //  }
 // }
 
+class Node {
+ constructor(value){
+  this.value = value;
+  this.next = null;
+ }
+}
+
 class LinkedList {
  constructor(value) {
-  this.head = {
-   value: value,
-   next: null
-  }
+  this.head = new Node(value)
 
   this.tail = this.head;
   this.length = 1;
  }
 
  append(value) {
-  const newNode = {
-   value: value,
-   next: null
-  }
+  const newNode = new Node(value)
 
   this.tail.next = newNode;
   this.tail = newNode;
@@ -36,10 +37,7 @@ class LinkedList {
  }
 
  prepend(value){
-  const newNode = {
-   value: value,
-   next: null
-  }
+  const newNode = new Node(value)
 
   newNode.next = this.head;
   this.head = newNode;
