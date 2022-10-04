330ff5bbf2b70c8cceeed617aa9117e4024334ed
kamal <kamade123@gmail.com>
2022-10-04 19:59:40 +0100

    built a class based  linkedlist

--- 3-linked-list.js
+++ 3-linked-list.js
@@ -1,14 +1,29 @@
-10-->5-->16
-let myLinkedList = {
- head: {
-  value: 10,
-  next:{
-   value: 5,
-   next:{
-    value: 16,
-    next: null
-   }
+// 10-->5-->16
+// let myLinkedList = {
+//  head: {
+//   value: 10,
+//   next:{
+//    value: 5,
+//    next:{
+//     value: 16,
+//     next: null
+//    }
+//   }
+//  }
+// }
+
+class LinkedList {
+ constructor(value) {
+  this.head = {
+   value: value,
+   next: null
   }
+
+  this.tail = this.head;
+  this.length = 1;
  }
 }
 
+const myLinkedList = new LinkedList(10);
+
+console.log(myLinkedList);
