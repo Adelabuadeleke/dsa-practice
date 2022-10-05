68d827571df54203402202150b4170eac636eed1
kamal <kamade123@gmail.com>
2022-10-05 01:59:36 +0100

    added  insert method to the  linkedlist

--- 3-linked-list.js
+++ 3-linked-list.js
@@ -44,18 +44,39 @@ class LinkedList {
   this.length++;
   // return console.log(this)
  }
-printList(){
- const array = [];
- let currentNode = this.head;
- while(currentNode !== null){
-  array.push(currentNode.value);
-  currentNode = currentNode.next;
+
+ printList(){
+  const array = [];
+  let currentNode = this.head;
+  while(currentNode !== null){
+   array.push(currentNode.value);
+   currentNode = currentNode.next;
+  }
+  return console.log(array);
  }
- return console.log(array);
-}
 
  insert(index, value) {
+  if(index >= this.length){
+   return this.append(value)
+  }
 
+  const newNode = new Node(value);
+  const leader = this.traverseToindex(index - 1);
+  const holdingPointer = leader.next;
+  leader.next = newNode
+  newNode.next = holdingPointer;
+  this.length++
+  return this.printList();
+ }
+
+ traverseToindex(index) {
+  let counter = 0;
+  let currentNode = this.head;
+  while(counter !== index) {
+   currentNode = currentNode.next;
+   counter++;
+  }
+  return currentNode;
  }
 
 }
@@ -64,5 +85,6 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1)
-// myLinkedList.insert(2, 99)
+myLinkedList.insert(2, 99)
+myLinkedList.insert(20, 88)
 myLinkedList.printList();
