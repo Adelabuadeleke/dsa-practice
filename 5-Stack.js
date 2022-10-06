1b1a8c9f0331ac8c0f8f5f7919d7a37456926033
kamal <kamade123@gmail.com>
2022-10-06 18:11:11 +0100

    created a class streucture for Stack

---
+++ 5-Stack.js
@@ -0,0 +1,31 @@
+class Node {
+ constructor(value) {
+  this.value = value;
+  this.next = null;
+ }
+}
+
+class Stack {
+ constructor() {
+  this.top = null;
+  this.bottom = null;
+  this.length = 0;
+ }
+
+ peek() {
+
+ }
+
+ push(value) {
+
+ }
+
+ pop() {
+
+ }
+
+ isEmpty() {
+
+ }
+ 
+}
