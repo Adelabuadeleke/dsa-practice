c084e5a3165de99743b1c316038e1e9a796a39a3
kamal <kamade123@gmail.com>
2022-10-06 21:11:24 +0100

    enqueue, dequeue, isEmpty method functionality added

--- 7-Queue.js
+++ 7-Queue.js
@@ -16,12 +16,38 @@ class Queue {
   return console.log(this.first)
  }
 
- enqueue(value){
+ enqueue(value) {
+  const newNode = new Node(value)
+  if(this.length === 0){
+   this.first = newNode;
+   this.last = newNode;
+   this.length++;
+  } else {
+   const holdingPointer = this.last;
+   console.log(holdingPointer);
+   holdingPointer.next = newNode;
+   this.last = newNode;
+   this.length++;
+  }
 
+  return console.log(this);
  }
 
  dequeue(){
-
+  if(this.length === 0) {
+   return console.log('cannot dequeue an empty Queue!')
+  } else if (this.first === this.last) {
+   this.first = null;
+   this.last = null;
+   this.length--;
+  } else {
+   const holdingPointer = this.first.next;
+   delete this.first;
+   this.first = holdingPointer;
+   this.first.next = holdingPointer.next;
+   this.length--
+  }
+  return console.log(this);
  }
 
  isEmpty(){
@@ -34,7 +60,13 @@ class Queue {
 }
 
 const myQueue = new Queue();
-
+myQueue.isEmpty();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.enqueue('Samir');
+myQueue.peek();
+myQueue.dequeue();
 // Joy
 // Matt
 // Pavel
