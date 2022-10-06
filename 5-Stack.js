c539e35a94e73c5e4456a2341ce85664812932e1
kamal <kamade123@gmail.com>
2022-10-06 18:56:49 +0100

    added pop and isEmpty methods to the Stack

--- 5-Stack.js
+++ 5-Stack.js
@@ -31,14 +31,33 @@ class Stack {
  }
 
  pop() {
+  if(!this.top) {
+   return console.log(null)
+  }
 
- }
+  const holdingPointer = this.top;
+  this.top = this.top.next;
+  this.length--;
+  return console.log(holdingPointer);
+ } 
 
  isEmpty() {
-
+  if(this.length === 0){
+   return console.log(true)
+  } else {
+   console.log(false)
+  }
  }
-
 }
 
 const myStack = new Stack();
+myStack.isEmpty();
 myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.isEmpty()
+myStack.peek()
+myStack.pop();
+myStack.pop();
+myStack.pop();
+myStack.isEmpty();
