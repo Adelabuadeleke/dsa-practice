1a2d49d9e41333e94f3bd6ad7be0f4b922686fcb
kamal <kamade123@gmail.com>
2022-10-08 18:58:58 +0100

    added graph: graph data structure

---
+++ 10-graph.js
@@ -0,0 +1,13 @@
+// Edge List
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]];
+
+// Adjacent List
+const graph2 = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// Adjacent Matrix
+const graph3 = {
+ 0: [0, 0, 1, 0],
+ 1: [0, 0, 1, 1],
+ 2: [1, 1, 0, 1],
+ 3: [0, 1, 1, 0]
+}
