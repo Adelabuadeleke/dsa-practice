aa1705879abaf251948cd93c55cd0e0ad2e85a8c
kamal <kamade123@gmail.com>
2022-10-09 18:16:49 +0100

    find factorial iterative function added

--- Algorithms/1-Recursion.js
+++ Algorithms/1-Recursion.js
@@ -8,4 +8,26 @@ function inception() {
  return inception();
 }
 
-inception();
+// inception();
+
+// Write two functions that finds factorial of any number. 
+// One should use recursive, the other should just use a for loop.
+
+function findFactorialRecursive(number) {
+ 
+}
+
+function findFactorialiterative(number) {
+ let answer = 1;
+
+ if(number === 2){
+  answer = 2;
+ }
+ for(let i = 2; i <= number; i++) {
+  answer = answer * i;
+ }
+
+ return console.log(answer);
+}
+
+findFactorialiterative(1)
