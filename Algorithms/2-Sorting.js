df3a9484b46470824a034854a5dda2fa48c5d8a6
kamal <kamade123@gmail.com>
2022-10-10 19:26:10 +0100

    selection sort algorithm

--- Algorithms/2-Sorting.js
+++ Algorithms/2-Sorting.js
@@ -27,5 +27,26 @@ function bubbleSort(array) {
  }
 }
 
-bubbleSort(numbers);
+// selection sort
+function selectionSort(array) {
+ const length = array.length;
+ let sorted = []
+ for(let i = 0; i < length; i++) {
+  // set current index as minmum
+  let min = i;
+  let temp = array[i];
+  for(let j = i+1; j < length; j++) {
+   if(array[j] < array[min]){
+    min = j;
+   }
+  }
+  array[i] = array[min];
+  array[min] = temp;
+ }
+ return array;
+}
+
+
+// bubbleSort(numbers);
+selectionSort(numbers)
 console.log(numbers);
