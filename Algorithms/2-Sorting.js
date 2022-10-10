a5d62cb341549851c5e27e223180df2dd436e587
kamal <kamade123@gmail.com>
2022-10-10 18:54:13 +0100

    bubble sort algorithm

--- Algorithms/2-Sorting.js
+++ Algorithms/2-Sorting.js
@@ -6,5 +6,26 @@ const newBask = letters.sort((a, b)=>{
 });
 const newLet = letters.sort();
 
-console.log(newBask);
-console.log(newLet);
+// console.log(newBask);
+// console.log(newLet);
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 8];
+
+// Bubble Sort
+function bubbleSort(array) {
+ const length = array.length;
+ for  (let i = 0; i < length; i++ ){
+  for(let j = 0; j < length; j++){
+   if(array[j] > array[j + 1]) {
+    // swap numbers 
+    let temp = array[j];
+    array[j] = array[j+1];
+    array[j + 1] = temp;
+   }
+   
+  }
+ }
+}
+
+bubbleSort(numbers);
+console.log(numbers);
