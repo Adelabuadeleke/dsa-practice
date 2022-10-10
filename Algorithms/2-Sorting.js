8792d0341fa52879c1bb1f70f1751fd349962d66
kamal <kamade123@gmail.com>
2022-10-10 20:14:17 +0100

    insertion sort algorithm

--- Algorithms/2-Sorting.js
+++ Algorithms/2-Sorting.js
@@ -30,7 +30,6 @@ function bubbleSort(array) {
 // selection sort
 function selectionSort(array) {
  const length = array.length;
- let sorted = []
  for(let i = 0; i < length; i++) {
   // set current index as minmum
   let min = i;
@@ -46,7 +45,25 @@ function selectionSort(array) {
  return array;
 }
 
+// insertion sort
+function insertionSort(array) {
+ const length = array.length;
+ for(let i = 0; i < length; i++){
+  if(array[i] < array[0]) {
+   // move number to the first position
+   array.unshift(array.splice(i, 1)[0]);
+  } else {
+   // find where number should go 
+   for(let j = 1; j < i; j++) {
+    if(array[i] > array[j-1] && array[i] < array[j]) {
+     // move number to the right spot
+     array.splice(j, 0, array.splice(i, 1)[0]);
+    }
+   }
+  }
+ }
+}
 
 // bubbleSort(numbers);
-selectionSort(numbers)
+insertionSort(numbers)
 console.log(numbers);
