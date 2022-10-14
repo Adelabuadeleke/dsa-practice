9eccd95982655a1bb8e681d681fd72b5d7c661a7
kamal <kamade123@gmail.com>
2022-10-14 21:11:01 +0100

    searching algo intro

---
+++ Algorithms/3-Searching.js
@@ -0,0 +1,12 @@
+var beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie'];
+
+beasts.indexOf('Godzilla');
+
+beasts.findIndex((item) =>{
+ return item === 'Godzilla';
+})
+beasts.find((item)=>{
+ return item === 'Godzilla';
+})
+
+beasts.includes('Godzilla')
